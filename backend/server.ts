080f6f28c595613c5fc47b809df0b9c0e21f8f80
Akhtar shaikh <mdak3335@gmail.com>
2026-05-22 19:56:57 +0530

    url updated in cors

--- backend/server.ts
+++ backend/server.ts
@@ -22,7 +22,7 @@ const app = express();
 
 // midd
 app.use(cors({
-    origin:['http://localhost:5173',],
+    origin:['http://localhost:5173','https://thumbcraftai-five.vercel.app'],
     credentials:true
 }));
 app.use(session({
